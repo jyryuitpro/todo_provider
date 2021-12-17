339c03935f2fe2839baf243252a7752f7bec60d5
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-17 20:26:00 +0900

    Packages, folders, 앱 skeleton

---
+++ lib/pages/todos_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TodosPage extends StatefulWidget {
+  const TodosPage({Key? key}) : super(key: key);
+
+  @override
+  _TodosPageState createState() => _TodosPageState();
+}
+
+class _TodosPageState extends State<TodosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('ToDo Provider'),
+      ),
+    );
+  }
+}
