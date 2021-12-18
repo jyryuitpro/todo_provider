a3190a62766ca3a37bb3b9b41c52a027fd73c354
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-18 11:53:28 +0900

    Injecting providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_provider/pages/todos_page.dart';
+import 'package:todo_provider/providers/active_todo_count.dart';
+import 'package:todo_provider/providers/filtered_todos.dart';
+import 'package:todo_provider/providers/todo_filter.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+import 'package:todo_provider/providers/todo_search.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +17,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ToDo Provider',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (context) => TodoFilter(),
+        ),
+        ChangeNotifierProvider<TodoSearch>(
+          create: (context) => TodoSearch(),
+        ),
+        ChangeNotifierProvider<TodoList>(
+          create: (context) => TodoList(),
+        ),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(),
+          update: (
+            BuildContext context,
+            TodoList todoList,
+            ActiveTodoCount? activeTodoCount,
+          ) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(),
+          update: (BuildContext context,
+                  TodoFilter todoFilter,
+                  TodoSearch todoSearch,
+                  TodoList todoList,
+                  FilteredTodos? filteredTodos) =>
+              filteredTodos!..update(todoFilter, todoSearch, todoList),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'ToDo Provider',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const TodosPage(),
       ),
-      home: const TodosPage(),
     );
   }
 }
