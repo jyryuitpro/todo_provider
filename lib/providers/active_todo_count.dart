114214a3ac9ff598cba6d409f907582cabdb0cb3
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-18 10:43:15 +0900

    ActiveTodoCountState and ActiveTodoCount ChangeNotifier

---
+++ lib/providers/active_todo_count.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:todo_provider/models/todo_model.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+
+class ActiveTodoCountState extends Equatable {
+  final int activeTodoCount;
+
+  ActiveTodoCountState({
+    required this.activeTodoCount,
+  });
+
+  factory ActiveTodoCountState.initial() {
+    return ActiveTodoCountState(activeTodoCount: 0);
+  }
+
+  @override
+  List<Object?> get props => [activeTodoCount];
+
+  @override
+  bool get stringify => true;
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+}
+
+class ActiveTodoCount with ChangeNotifier {
+  ActiveTodoCountState _state = ActiveTodoCountState.initial();
+
+  ActiveTodoCountState get state => _state;
+
+  void update(TodoList todoList) {
+    final int newActiveTodoCount = todoList.state.todos
+        .where((Todo todo) => !todo.completed)
+        .toList()
+        .length;
+
+    _state = _state.copyWith(activeTodoCount: newActiveTodoCount);
+    notifyListeners();
+  }
+}
